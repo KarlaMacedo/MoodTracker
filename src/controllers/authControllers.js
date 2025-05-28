b84129249861feb0800b50e0e7d105dfec30e6fa
KarlaMacedo <karlapaolamacedo@gmail.com>
2025-05-28 17:20:33 -0600

    Update separate auth routes and controllers

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/Users.js";
+
+export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "El usuario ya existe" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        res.status(201).json({ token, user: { id: user._id, username: user.username, email: user.email  } });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error del servidor" });
+    }
+};
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Credenciales inválidas" });
+        }
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Credenciales inválidas" });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+        res.status(200).json({ token, user: { id: user._id, username: user.username, email: user.email  } });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error del servidor" });
+    }
+};
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,51 +1,9 @@
 import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/Users.js";
+import { register, login } from "../controllers/authController.js";
 
 const router = express.Router();
 
-//POST register
-router.post("/register", async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: "El usuario ya existe" });
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({
-            username,
-            email,
-            password: hashedPassword,
-        });
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-        res.status(201).json({ token, user: { id: user._id, username: user.username, email: user.email  } });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error del servidor" });
-    }
-});
-
-
-//POST login
-router.post("/login", async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ email });
-        if (!user) {
-            return res.status(401).json({ message: "Credenciales inválidas" });
-        }
-        const isPasswordValid = await user.comparePassword(password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: "Credenciales inválidas" });
-        }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-        res.status(200).json({ token, user: { id: user._id, username: user.username, email: user.email  } });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Error del servidor" });
-    }
-});
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
