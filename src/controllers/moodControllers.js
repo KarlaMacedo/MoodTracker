992b1dc787d0f2df823b057eb6e51afe808d5316
KarlaMacedo <karlapaolamacedo@gmail.com>
2025-07-02 15:26:34 -0600

    Update add date-fns-tz

--- src/controllers/moodControllers.js
+++ src/controllers/moodControllers.js
@@ -1,4 +1,5 @@
 import Mood from "../models/Mood.js";
+import { zonedTimeToUtc } from "date-fns-tz";
 
 export const createMood = async (req, res) => {
     try {
@@ -14,45 +15,50 @@ export const createMood = async (req, res) => {
 export const getMoods = async (req, res) => {
     try {
         const { tag, startDate, endDate, page = 1, limit = 10 } = req.query;
+
         const query = { user: req.user._id };
-        console.log(req.query);
+        const timeZone = 'America/Mexico_City';
+
+        console.log("Query recibido:", req.query);
+
         if (tag) query.tag = tag;
-        const offsetHours = 6;
+
         if (startDate || endDate) {
             query.createdAt = {};
+
             if (startDate) {
-                const start = new Date(startDate);
-                start.setUTCHours(0 + offsetHours, 0, 0, 0);
-                query.createdAt.$gte = new Date(start);
+                const start = zonedTimeToUtc(`${startDate}T00:00:00`, timeZone);
+                query.createdAt.$gte = start;
             }
 
             if (endDate) {
-                const end = new Date(endDate);
-                end.setUTCHours(23 + offsetHours, 59, 59, 999);
-                query.createdAt.$lte = new Date(end);
+                const end = zonedTimeToUtc(`${endDate}T23:59:59`, timeZone);
+                query.createdAt.$lte = end;
             }
         }
-        console.log(query, "queryfinal");
+
+        console.log("Query final:", query);
 
         const skip = (parseInt(page) - 1) * parseInt(limit);
+
         const [total, moods] = await Promise.all([
             Mood.countDocuments(query),
             Mood.find(query).sort({ createdAt: -1 }).limit(parseInt(limit)).skip(skip)
         ]);
+
         const totalPages = Math.ceil(total / parseInt(limit));
+
         res.status(200).json({
             moods,
             pagination: {
                 total,
                 totalPages,
                 currentPage: parseInt(page),
-                perPage: parseInt(limit)
-            }
+                perPage: parseInt(limit),
+            },
         });
-
-        console.log(moods);
     } catch (error) {
-        console.error(error);
+        console.error("Error al obtener los registros:", error);
         res.status(500).json({ message: "Error al obtener los registros emocionales" });
     }
 };
